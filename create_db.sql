36ad53d35b35fb5d2375ccf08e20bc442e3809f3
SimranJohal2002 <sjoha003@gold.ac.uk>
2023-12-10 16:25:23 +0000

    changed taskmanager/login/registration page

--- create_db.sql
+++ create_db.sql
@@ -1,6 +1,24 @@
-CREATE DATABASE taskmanager;
+CREATE DATABASE IF NOT EXISTS taskmanager;
 USE taskmanager;
-CREATE TABLE manager (id INT AUTO_INCREMENT PRIMARY KEY,taskname VARCHAR(100),taskdescription VARCHAR(100),dateadded DATETIME, datecompletion DATETIME);
-CREATE TABLE userdetails (userid INT AUTO_INCREMENT PRIMARY KEY,username VARCHAR(100),firstname VARCHAR(100),lastname VARCHAR(100),email VARCHAR(100),hashedPassword VARCHAR(200));
+
+CREATE TABLE IF NOT EXISTS user (
+    userid INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(100),
+    firstname VARCHAR(100),
+    lastname VARCHAR(100),
+    email VARCHAR(100),
+    hashedPassword VARCHAR(200)
+);
+
+CREATE TABLE IF NOT EXISTS task (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    taskname VARCHAR(100),
+    taskdescription VARCHAR(100),
+    dateadded DATETIME,
+    datecompletion DATETIME,
+    userid INT,
+    FOREIGN KEY (userid) REFERENCES user(userid)
+);
+
 CREATE USER 'appuser'@'localhost' IDENTIFIED WITH mysql_native_password BY 'app2027';
-GRANT ALL PRIVILEGES ON myBookshop.* TO 'appuser'@'localhost';
+GRANT ALL PRIVILEGES ON taskmanager.* TO 'appuser'@'localhost';
