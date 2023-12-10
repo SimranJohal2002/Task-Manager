364786a6adfed3b51590d6057f40fb61291434bc
SimranJohal2002 <sjoha003@gold.ac.uk>
2023-12-10 16:22:03 +0000

    changed index.js

--- index.js
+++ index.js
@@ -1,63 +1,73 @@
-// Import the modules we need
-var express = require ('express')
-var ejs = require('ejs')
-var bodyParser= require ('body-parser')
+var express = require('express');
+var ejs = require('ejs');
+var bodyParser = require('body-parser');
+const session = require('express-session');
 const mysql = require('mysql');
+const fs = require('fs');
 
-// Create the express application object
-const app = express()
-const port = 8000
-app.use(bodyParser.urlencoded({ extended: true }))
+const expressSanitizer = require('express-sanitizer');
 
-// Set up css
+const app = express();
+const port = 8000;
+
+app.use(expressSanitizer());
+
+app.use(
+  session({
+    secret: 'testsecretkey',
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/public'));
 
-// Define the database connection
-const db = mysql.createConnection ({
-    host: 'localhost',
-    user: 'appuser',
-    password: 'app2027',
-    database: 'taskmanager'
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'appuser',
+  password: 'app2027',
+  multipleStatements: true,
 });
-// Connect to the database
+
 db.connect((err) => {
+  if (err) {
+    throw err;
+  }
+
+  console.log('Connected to MySQL server');
+
+  const createDatabaseScript = fs.readFileSync(
+    __dirname + '/create_db.sql',
+    'utf8'
+  );
+
+  db.query(createDatabaseScript, (err, results) => {
     if (err) {
-        throw err;
+      throw err;
     }
-    console.log('Connected to database');
+
+    console.log('Database and tables created successfully');
+
+    db.changeUser({ database: 'taskmanager' }, (err) => {
+      if (err) {
+        throw err;
+      }
+
+      console.log('Connected to taskmanager database');
+
+      app.set('views', __dirname + '/views');
+      app.set('view engine', 'ejs');
+      app.engine('html', ejs.renderFile);
+
+      var appData = { appName: 'Task Manager' };
+      require('./routes/main')(app, appData);
+
+      app.listen(port, () =>
+        console.log(`Example app listening on port ${port}!`)
+      );
+    });
+  });
 });
+
 global.db = db;
-
-
-// Set the directory where Express will pick up HTML files
-// __dirname will get the current directory
-app.set('views', __dirname + '/views');
-
-// Tell Express that we want to use EJS as the templating engine
-app.set('view engine', 'ejs');
-
-// Tells Express how we should process html files
-// We want to use EJS's rendering engine
-app.engine('html', ejs.renderFile);
-
-// Define our data
-var appData = {appName: "Task Manager"}
-
-// Requires the main.js file inside the routes folder passing in the Express app and data as arguments.  All the routes will go in this file
-require("./routes/main")(app, appData);
-
-// Start the web app listening
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
-
-
-// document.addEventListener('DOMContentLoaded', function (){
-//     loadHTMLTable([]);
-// });
-
-// function loadHTMLTable(data) {
-//     const table = document.querySelector('table tbody');
-
-//     if(data.length === 0) {
-//         table.innerHTML = "<tr><td class='no-data' colspan='5'>No Data</td></tr>";
-//     }
-// }
